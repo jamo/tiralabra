248fb13255a94c605fa1544bc00aca37868c21c6
Jarmo Isotalo <jamo@isotalo.fi>
2012-09-28 14:47:39 +0300

    siivous meneillään

--- heaps/Heap/spec/spec_helper.rb
+++ heaps/Heap/spec/spec_helper.rb
@@ -1,11 +1,10 @@
 require 'simplecov'
 require 'pp'
-SimpleCov.start
-require_relative '../src/BinaryHeap.rb'
-require_relative '../src/d_heap.rb'
-require_relative '../src/binomial_heap.rb'
-
 require "rspec"
+
+SimpleCov.start
+require_relative '../src/binary_heap.rb'
+require_relative '../src/d_heap.rb'
 
 
 
---
+++ heaps/Heap/src/old/binomial_heap.rb
@@ -0,0 +1,9 @@
+require 'pp'
+class BinaryHeap < DHeap
+#maksimi binomikeko
+
+  def initialize
+    super 2
+  end
+
+end
