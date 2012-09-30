7d030c983818cd05899d9f1412e407e493bcfc6b
Jarmo Isotalo <jamo@isotalo.fi>
2012-09-30 19:55:02 +0300

    Dokumentaatiota

--- heaps/Heap/src/stack.rb
+++ heaps/Heap/src/stack.rb
@@ -1,26 +1,35 @@
+#Oma pino implementaatio
 class Stack
 
+  #Pinolla on stack taulukko
   attr_accessor :stack
+  
+  #Alustetaan pino
   def initialize
     @stack = Array.new
   end
 
+  #Lisätään elementti pinon
   def push(item)
     @stack.push item
   end
 
+  #Poimitaan elementti pinosta
   def pop
     @stack.pop
   end
 
+  #Palautta pinon pituden
   def count
     @stack.length
   end
 
+  #Tyhjentää pinon
   def clear
     @stack.clear
   end
-
+  
+  #Kertoo onko pino tyhjä
   def empty?
     @stack.empty?
   end
